deb84a64169265250e70e59bddc260e7c26e1362
kevorklepedjian1 <99024590+kevorklepedjian1@users.noreply.github.com>
2023-12-10 19:27:23 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav>
       <div className='container flex items-center justify-between mx-auto w-full'>
         <img src='../assets/logo-dark.svg' alt='Dark Logo' className='' />
-        <div className='inline-block lg:py-3 lg:px-8 md:py-3 md:px-8 bg-darkBlue text-white rounded-3xl cursor-pointer hover:bg-lightBlue sm:py-1 sm:px-2'>
+        <div className='inline-block py-3 px-8 bg-darkBlue text-white rounded-3xl cursor-pointer hover:bg-lightBlue '>
           <a >Get Started</a>
         </div>
       </div>
